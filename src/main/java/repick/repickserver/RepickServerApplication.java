a139c1ec54e49b0e153fb212f6e6abbf12b7ad00
seohyejun <shyejun0718@naver.com>
2023-06-30 14:09:38 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/repick/repickserver/RepickServerApplication.java
@@ -0,0 +1,13 @@
+package repick.repickserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepickServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepickServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/repick/repickserver/RepickServerApplicationTests.java
@@ -0,0 +1,13 @@
+package repick.repickserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepickServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
