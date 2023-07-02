d6b48f2a766077a900fa2637dc804513fa497e14
seochanhyeok <mushroom1324@naver.com>
2023-07-02 19:34:00 +0900

    Fix: "변경 깃헙 액션 수정"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,6 @@ if ! type docker-compose > /dev/null
 then
   echo "docker-compose does not exist"
   echo "Start installing docker-compose"
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-echo "start docker-compose up: ubuntu"
-sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yaml up --build -d
