cdc632633c73797eede6ae329289796a857d8304
seochanhyeok <mushroom1324@naver.com>
2023-07-05 10:56:29 +0900

    Fix: "변경 created_at, updated_at 설정"

--- src/main/java/repick/repickserver/domain/member/domain/Member.java
+++ src/main/java/repick/repickserver/domain/member/domain/Member.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
     @Id
---
+++ src/main/java/repick/repickserver/domain/order/domain/Order.java
@@ -0,0 +1,17 @@
+package repick.repickserver.domain.order.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+//
+//    @Column
+//    private
+
+}
