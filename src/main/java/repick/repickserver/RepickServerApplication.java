1a5061e21773eaa54bfddd8af85f7753df2b50df
seochanhyeok <mushroom1324@naver.com>
2023-07-12 18:24:48 +0900

    Fix: "수정 CORS 설정"

--- src/main/java/repick/repickserver/RepickServerApplication.java
+++ src/main/java/repick/repickserver/RepickServerApplication.java
@@ -3,10 +3,7 @@ package repick.repickserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import repick.repickserver.global.config.JwtProperties;
 
 @SpringBootApplication
@@ -16,16 +13,6 @@ public class RepickServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepickServerApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 
 }
