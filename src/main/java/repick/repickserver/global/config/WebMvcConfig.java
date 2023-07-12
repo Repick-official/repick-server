b315dfd5b273150800a89782614daeb578f88505
seochanhyeok <mushroom1324@naver.com>
2023-07-12 18:31:48 +0900

    Fix: "수정 CORS 설정"

--- src/main/java/repick/repickserver/global/config/WebMvcConfig.java
+++ src/main/java/repick/repickserver/global/config/WebMvcConfig.java
@@ -1,19 +1,24 @@
 package repick.repickserver.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableScheduling
+@EnableTransactionManagement
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
+                    .allowCredentials(true);
     }
 
 }
