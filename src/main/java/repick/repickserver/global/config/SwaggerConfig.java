e36769c4549fabaec98e12c3cc00e93c1c58ca70
seochanhyeok <mushroom1324@naver.com>
2023-07-13 17:07:31 +0900

    Feat: "추가 스웨거 로컬 및 배포 서버 설정 가능"

--- src/main/java/repick/repickserver/global/config/SwaggerConfig.java
+++ src/main/java/repick/repickserver/global/config/SwaggerConfig.java
@@ -1,22 +1,38 @@
 package repick.repickserver.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final ServerProperties serverProperties;
 
     @Bean
     public Docket api() {
+
+
+        Server local = new Server("local", "http://localhost:8080", "local server",
+                Collections.emptyList(), Collections.emptyList());
+
+        Server develop = new Server("develop", serverProperties.getServerAddress(), "develop server",
+                Collections.emptyList(), Collections.emptyList());
+
         return new Docket(DocumentationType.OAS_30)
+                .servers(local, develop)
                 .useDefaultResponseMessages(true)
                 .apiInfo(apiInfo())
                 .select()
---
+++ src/main/java/repick/repickserver/global/config/Workaround.java
@@ -0,0 +1,41 @@
+package repick.repickserver.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+
+@Component
+@RequiredArgsConstructor
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+    private final ServerProperties serverProperties;
+
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+
+        Server developServer = new Server();
+        developServer.setDescription("develop");
+        developServer.setUrl(serverProperties.getServerAddress());
+        openApi.setServers(Arrays.asList(localServer, developServer));
+
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
