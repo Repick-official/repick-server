b627458b7ce3cd211e3973e4fd12f436c3a9bdff
seochanhyeok <mushroom1324@naver.com>
2023-07-14 13:39:26 +0900

    Fix: "수정 머지 충돌 해결"

--- src/main/java/repick/repickserver/domain/product/application/ProductService.java
+++ src/main/java/repick/repickserver/domain/product/application/ProductService.java
@@ -101,6 +101,7 @@ public class ProductService {
                                 .orElseThrow(() -> new CustomException(PRODUCT_MAIN_IMAGE_NOT_FOUND)))
                         .build())
                 .collect(Collectors.toList());
+    }
       
     /**
      * <h1>상품 상세 조회</h1>
@@ -120,9 +121,9 @@ public class ProductService {
                         .filter(ProductImage::getIsMainImage)
                         .findFirst()
                         .orElseThrow(() -> new CustomException(PRODUCT_IMAGE_NOT_FOUND)))
-                .detailProductImages(productImages.stream()
+                        .detailProductImages(productImages.stream()
                         .filter(productImage -> !productImage.getIsMainImage())
                         .collect(Collectors.toList()))
-                .build();
+                        .build();
     }
 }
