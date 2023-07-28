a04d690de9fa9a001f6bf61094e4c9092c53b0b5
seochanhyeok <mushroom1324@naver.com>
2023-07-28 11:07:48 +0900

    Feat: "추가 슬랙봇 서비스"

--- src/main/java/repick/repickserver/infra/slack/api/SlackApiController.java
+++ src/main/java/repick/repickserver/infra/slack/api/SlackApiController.java
@@ -2,6 +2,7 @@ package repick.repickserver.infra.slack.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repick.repickserver.infra.slack.application.SlackApiService;
@@ -15,12 +16,12 @@ public class SlackApiController {
     private final SlackApiService slackApiService;
 
     @PostMapping(value = "/subscribe/add")
-    public void addSubscribeRequest(SlackApiDto request) {
+    public void addSubscribeRequest(@RequestBody SlackApiDto request) {
         slackApiService.addSubscribeRequest(request);
     }
 
     @PostMapping(value = "/subscribe/deny")
-    public void denySubscribeRequest(SlackApiDto request) {
+    public void denySubscribeRequest(@RequestBody SlackApiDto request) {
         slackApiService.denySubscribeRequest(request);
     }
 
